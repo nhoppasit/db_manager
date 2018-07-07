f17fee53c720e0291dca20a15f12b99fa4389aac
Nhoppasit <dev.theprogram@gmail.com>
2018-07-07 10:03:28 +0700

    First commit

---
+++ DB Management/DB Security/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DB_Security
+{
+    public static class Settings
+    {
+        static string _connectionString = @"Data Source=SOMPHOP-PC\SQLEXPRESS;Initial Catalog=db_ncs_01;User ID=ncs_admin;Password=thunder@11";
+        //static string _connectionString = @"Data Source=BHMVISION\SQLEXPRESS;Initial Catalog=db_ncs_01;User ID=ncs_admin;Password=thunder@11";
+        public static string ConnectionString { get { return _connectionString; } }
+    }
+}
---
+++ DB Management/Test/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        LogFile.Log log;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string LogPath = string.Empty;
+            using (DB_Manager.ConfigManagement cm = new DB_Manager.ConfigManagement())
+            {
+                try { LogPath = cm.GetCharValue("Motion_Card_Log_Path"); }
+                catch { LogPath = @"C:\TestLog\Test"; }
+            }
+            log = new LogFile.Log(LogPath, "Test log");
+        }
+    }
+}
